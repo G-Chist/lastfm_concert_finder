f90cab19882a56c91905cd1ffa16ecb432fc498b
G-Chist <42654606+G-Chist@users.noreply.github.com>
2024-08-06 14:20:45 -0400

    Update concerts_for_artist.py

--- concerts_for_artist.py
+++ concerts_for_artist.py
@@ -1,19 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'https://www.last.fm/music/'
+BASE_URL = 'https://www.last.fm/'
+
 
 def get_upcoming_events(artist_name):
-    url = BASE_URL + artist_name + '/+events'
+    artist = artist_name.replace(" ", "+")
+    url = BASE_URL + 'music/' + artist + '/+events'
     response = requests.get(url)
     html_content = response.content
     soup = BeautifulSoup(html_content, 'html.parser')
     music_events = soup.find_all(attrs={"itemtype": "http://schema.org/MusicEvent"})
-    return music_events
+    links = []
+    for event in music_events:
+        event_link = event.find(class_="events-list-item-event-name link-block-target")
+        href = event_link.get('href')
+        links.append(href)
+    return links
+
 
 if __name__ == "__main__":
     # Example usage
-    artist_name = 'Omerta'
+    artist_name = 'Ice Nine Kills'
     events = get_upcoming_events(artist_name)
     print(events)
 
