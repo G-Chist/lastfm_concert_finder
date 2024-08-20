838fd33a042b28fa554537e6e5b24cd9fc53867c
G-Chist <42654606+G-Chist@users.noreply.github.com>
2024-08-20 02:24:32 -0400

    Update concerts_for_artist.py

--- concerts_for_artist.py
+++ concerts_for_artist.py
@@ -7,7 +7,13 @@ BASE_URL = 'https://www.last.fm/'
 def get_upcoming_events(artist_name):
     artist = artist_name.replace(" ", "+")
     url = BASE_URL + 'music/' + artist + '/+events'
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br'
+    }
+    response = requests.get(url, headers=headers)
     html_content = response.content
     soup = BeautifulSoup(html_content, 'html.parser')
     music_events = soup.find_all(attrs={"itemtype": "http://schema.org/MusicEvent"})
